fc891795d77802e475b1aba733be74ceabd79187
Malcolm St. John <malcolm19@hotmail.com>
2021-11-06 16:43:45 -0400

    setup and teardown complete

--- pkg/database/mongoDB_test.go
+++ pkg/database/mongoDB_test.go
@@ -2,16 +2,38 @@ package database
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/Ubivius/microservice-character-data/pkg/data"
 	"github.com/google/uuid"
 )
 
+func integrationTestSetup(t *testing.T) {
+	if os.Getenv("DB_USERNAME") == "" {
+		os.Setenv("DB_USERNAME", "admin")
+	}
+	if os.Getenv("DB_PASSWORD") == "" {
+		os.Setenv("DB_PASSWORD", "pass")
+	}
+	if os.Getenv("DB_PORT") == "" {
+		os.Setenv("DB_PORT", "27888")
+	}
+	if os.Getenv("DB_HOSTNAME") == "" {
+		os.Setenv("DB_HOSTNAME", "localhost")
+	}
+
+	err := deleteAllCharactersFromMongoDB()
+	if err != nil {
+		t.Errorf("Failed to delete existing characters from database during setup")
+	}
+}
+
 func TestMongoDBConnectionAndShutdownIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoCharacters()
 	if mp == nil {
@@ -24,6 +46,7 @@ func TestMongoDBAddCharacterIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	character := &data.Character{
 		Name:   "testName",
@@ -42,6 +65,7 @@ func TestMongoDBUpdateCharacterIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	character := &data.Character{
 		ID:     uuid.NewString(),
@@ -60,6 +84,7 @@ func TestMongoDBGetCharactersIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoCharacters()
 	characters := mp.GetCharacters(context.Background())
@@ -74,6 +99,7 @@ func TestMongoDBGetCharacterByIDIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoCharacters()
 	_, err := mp.GetCharacterByID(context.Background(), "a2181017-5c53-422b-b6bc-036b27c04fc8")
@@ -88,6 +114,7 @@ func TestMongoDBGetCharactersByUserIDIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Test skipped during unit tests")
 	}
+	integrationTestSetup(t)
 
 	mp := NewMongoCharacters()
 	_, err := mp.GetCharactersByUserID(context.Background(), "a2181017-5c53-422b-b6bc-036b27c04fc8")
